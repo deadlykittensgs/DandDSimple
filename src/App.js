087eb0c5b9374f7711a2e437792be89894f21437
deadlykittensgs <161607814+deadlykittensgs@users.noreply.github.com>
2025-07-24 18:42:18 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="flex bg-slate-300" >
+    <h1>D and D page </h1>
+ <input type="text" id="textInput" value="name"></input>
+  <input type="text" id="textInput" value="race"></input>
+   <input type="text" id="textInput" value="health"></input>
+    <input type="text" id="textInput" value="subclass"></input>
+     <input type="text" id="textInput" value="bla"></input>
+      <input type="text" id="textInput" value="bla"></input>
+       <input type="text" id="textInput" value="bla"></input>
+        <input type="text" id="textInput" value="bla"></input>
     </div>
   );
 }
